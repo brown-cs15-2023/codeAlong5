cda2482cd9fc3cd8b5db8074195095607dc2679a
Lenrion <90710471+Lenrion@users.noreply.github.com>
2023-05-15 17:30:31 -0400

    Initial commit

---
+++ PaneOrganizer.java
@@ -0,0 +1,20 @@
+package workshop5stencil;
+
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
+/**
+ * Organizes GUI layout for workshop5.
+ */
+public class PaneOrganizer {
+    private BorderPane root;
+
+    public PaneOrganizer() {
+        this.root = new BorderPane();
+    }
+
+    public Pane getRoot() {
+        return this.root;
+    }
+}
+
